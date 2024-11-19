137be850043a0a68649d5ed37f259b6fc16dfa4d
Nick Frasser <1693461+nfrasser@users.noreply.github.com>
2024-11-18 23:54:50 -0500

    Refactor CI workflow
    Should work with newest versions?

--- test/spec/linkify-react.test.mjs
+++ test/spec/linkify-react.test.mjs
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOMServer from 'react-dom/server';
+import { renderToStaticMarkup } from 'react-dom/server';
 import * as linkify from 'linkifyjs';
 import Linkify from 'linkify-react/src/linkify-react.mjs';
 import mention from 'linkify-plugin-mention/src/mention.mjs';
@@ -53,7 +53,7 @@ describe('linkify-react', () => {
 	it('Works with default options', function () {
 		tests.map((test) => {
 			var linkified = React.createElement(Linkify, null, test[0]);
-			var result = ReactDOMServer.renderToStaticMarkup(linkified);
+			var result = renderToStaticMarkup(linkified);
 			expect(result).to.be.oneOf([test[1], `<span>${test[1]}</span>`]);
 		});
 	});
@@ -62,7 +62,7 @@ describe('linkify-react', () => {
 		tests.map((test) => {
 			var props = { options, as: 'div', className: 'lambda' };
 			var linkified = React.createElement(Linkify, props, test[0]);
-			var result = ReactDOMServer.renderToStaticMarkup(linkified);
+			var result = renderToStaticMarkup(linkified);
 			expect(result).to.be.eql(test[2]);
 		});
 	});
@@ -70,7 +70,7 @@ describe('linkify-react', () => {
 	it('Finds links recursively', function () {
 		var strong = React.createElement('strong', null, 'https://facebook.github.io/react/');
 		var linkified = React.createElement(Linkify, null, 'A great site is google.com AND ', strong);
-		var result = ReactDOMServer.renderToStaticMarkup(linkified);
+		var result = renderToStaticMarkup(linkified);
 		var expected =
 			'A great site is <a href="http://google.com">google.com</a> AND <strong><a href="https://facebook.github.io/react/">https://facebook.github.io/react/</a></strong>';
 		expect(result).to.be.oneOf([expected, `<span>${expected}</span>`]);
@@ -85,7 +85,7 @@ describe('linkify-react', () => {
 
 		var delta = React.createElement(Delta);
 		var linkified = React.createElement(Linkify, null, 'A great site is google.com AND ', delta);
-		var result = ReactDOMServer.renderToStaticMarkup(linkified);
+		var result = renderToStaticMarkup(linkified);
 		var expected =
 			'A great site is <a href="http://google.com">google.com</a> AND <strong>https://facebook.github.io/react/</strong>';
 		expect(result).to.be.oneOf([expected, `<span>${expected}</span>`]);
@@ -97,7 +97,7 @@ describe('linkify-react', () => {
 		};
 		var em = React.createElement('em', null, 'https://facebook.github.io/react/');
 		var linkified = React.createElement(Linkify, { options }, 'A great site is google.com AND ', em);
-		var result = ReactDOMServer.renderToStaticMarkup(linkified);
+		var result = renderToStaticMarkup(linkified);
 		var expected =
 			'A great site is <a href="http://google.com">google.com</a> AND <em>https://facebook.github.io/react/</em>';
 		expect(result).to.be.oneOf([expected, `<span>${expected}</span>`]);
@@ -121,7 +121,7 @@ describe('linkify-react', () => {
 			'For the latest javascript.net\n',
 			React.createElement('strong', { key: 1 }, ['and also\n', '🥺👄.ws']),
 		);
-		const result = ReactDOMServer.renderToStaticMarkup(linkified);
+		const result = renderToStaticMarkup(linkified);
 		const expected = [
 			'hello<br/><br/>\t\t ',
 			'hello<br/><br/>\t\t',
@@ -155,7 +155,7 @@ describe('linkify-react', () => {
 				{ options },
 				'Check out linkify.js.org or contact @nfrasser',
 			);
-			const result = ReactDOMServer.renderToStaticMarkup(linkified);
+			const result = renderToStaticMarkup(linkified);
 			const expected =
 				'Check out <a href="http://linkify.js.org">linkify.js.org</a> or contact <span data-to="/users/nfrasser">@nfrasser</span>';
 			expect(result).to.be.oneOf([expected, `<span>${expected}</span>`]);
