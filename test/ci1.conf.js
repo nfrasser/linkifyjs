0f6cc246ea6c2022f0fb801420b65b2aa66af482
Nick Frasser <1693461+nfrasser@users.noreply.github.com>
2024-11-18 20:41:41 -0500

    Update CI matrices

--- test/ci1.conf.js
+++ test/ci1.conf.js
@@ -3,41 +3,40 @@
 const base = require('./conf');
 
 module.exports = function (config) {
-
 	// https://www.browserstack.com/docs/automate/api-reference/selenium/introduction#rest-api-browsers
 	const customLaunchers = {
 		bs_chrome_mac: {
 			base: 'BrowserStack',
 			browser: 'chrome',
 			os: 'OS X',
-			os_version: 'Monterey'
+			os_version: 'Ventura',
 		},
 		bs_chrome_windows: {
 			base: 'BrowserStack',
 			browser: 'chrome',
 			os: 'Windows',
-			os_version: '10'
+			os_version: '10',
 		},
 		bs_firefox_windows: {
 			base: 'BrowserStack',
 			browser: 'firefox',
 			os: 'Windows',
-			os_version: '10'
+			os_version: '10',
 		},
 		bs_android_8: {
 			base: 'BrowserStack',
 			os: 'android',
 			os_version: '9.0',
 			browser: 'android',
-			device: 'Google Pixel 3',
+			device: 'Pixel 3',
 		},
 		bs_android_11: {
 			base: 'BrowserStack',
 			os: 'android',
 			os_version: '11.0',
 			browser: 'android',
-			device: 'Google Pixel 5',
-		}
+			device: 'Pixel 5',
+		},
 	};
 
 	config.set({
@@ -52,7 +51,7 @@ module.exports = function (config) {
 			username: process.env.BROWSERSTACK_USERNAME,
 			accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
 			name: process.env.GITHUB_WORKFLOW,
-			build: process.env.GITHUB_RUN_NUMBER
+			build: process.env.GITHUB_RUN_NUMBER,
 		},
 
 		customLaunchers,
