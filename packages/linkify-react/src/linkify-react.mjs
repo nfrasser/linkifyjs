68b7f94c0849df09f90475d287b8e6ff5f9bc86e
Nick Frasser <1693461+nfrasser@users.noreply.github.com>
2025-07-25 13:42:55 -0400

    Remove unused import

--- packages/linkify-react/src/linkify-react.mjs
+++ packages/linkify-react/src/linkify-react.mjs
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { tokenize, Options, options } from 'linkifyjs';
+import { tokenize, Options } from 'linkifyjs';
 
 /**
  * Given a string, converts to an array of valid React components
